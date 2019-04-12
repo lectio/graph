01175ac3c0d2b3bdf9b4e04fb5050093afe8512f
Shahid Shah <shahid@shah.org>
2019-04-12 07:13:26 -0400

    Delegate to Resource

--- model/url.go
+++ model/url.go
@@ -3,15 +3,11 @@ package model
 import (
 	io "io"
 	"net/url"
-	"regexp"
 
 	"github.com/lectio/link"
 
 	graphql "github.com/99designs/gqlgen/graphql"
 )
-
-var defaultWebPrefixRegEx = regexp.MustCompile(`^www.`)                 // Removes "www." from start of source links
-var defaultTopLevelDomainSuffixRegEx = regexp.MustCompile(`\.[^\.]+?$`) // Removes ".com" and other TLD suffixes from end of hostname
 
 type URLText string
 type URL url.URL
@@ -32,16 +28,19 @@ func (t *URLText) UnmarshalGQL(v interface{}) error {
 // SimplifiedHostname returns the URL's hostname without 'www.' prefix
 func (t URLText) SimplifiedHostname() string {
 	u, e := url.Parse(string(t))
-	if e != nil {
-		return defaultWebPrefixRegEx.ReplaceAllString(u.Hostname(), "")
+	if e == nil {
+		return link.GetSimplifiedHostname(u)
 	}
 	return string(t)
 }
 
 // SimplifiedHostnameWithoutTLD returns the URL's hostname without 'www.' prefix and removes the top level domain suffix (.com, etc.)
 func (t URLText) SimplifiedHostnameWithoutTLD() string {
-	simplified := t.SimplifiedHostname()
-	return defaultTopLevelDomainSuffixRegEx.ReplaceAllString(simplified, "")
+	u, e := url.Parse(string(t))
+	if e == nil {
+		return link.GetSimplifiedHostnameWithoutTLD(u)
+	}
+	return string(t)
 }
 
 func (t URL) MarshalGQL(w io.Writer) {
@@ -62,13 +61,13 @@ func (t *URL) UnmarshalGQL(v interface{}) error {
 // SimplifiedHostname returns the URL's hostname without 'www.' prefix
 func (t URL) SimplifiedHostname() string {
 	u := url.URL(t)
-	return defaultWebPrefixRegEx.ReplaceAllString(u.Hostname(), "")
+	return link.GetSimplifiedHostname(&u)
 }
 
 // SimplifiedHostnameWithoutTLD returns the URL's hostname without 'www.' prefix and removes the top level domain suffix (.com, etc.)
 func (t URL) SimplifiedHostnameWithoutTLD() string {
-	simplified := t.SimplifiedHostname()
-	return defaultTopLevelDomainSuffixRegEx.ReplaceAllString(simplified, "")
+	u := url.URL(t)
+	return link.GetSimplifiedHostnameWithoutTLD(&u)
 }
 
 func (t Resource) MarshalGQL(w io.Writer) {
